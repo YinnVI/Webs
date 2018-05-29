5813571547fc82bac296688cfa34372ef28af7cc
YinnVI <yosia.kefas@gmail.com>
2018-05-29 13:41:25 +0700

    Update ScriptSafeLinkDownload.js

--- ScriptSafeLinkDownload.js
+++ ScriptSafeLinkDownload.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="ticketek-cto-says-chinese-style-facial.html"}
 if(x=="2"){xxx="big-techs-fight-for-net-neutrality.html"}
 if(x=="3"){xxx="subject-tiff-in-tech-course.html"}
 if(x=="4"){xxx="free-tech-on-offer-to-block-out.html"}
-if(x=="5"){xxx="tp18-panelists-discuss-navigating-tech.html"}a_to_ve[i].href="https://yuilinks.blogspot.com/2018/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="tp18-panelists-discuss-navigating-tech.html"}a_to_ve[i].href="https://swp.yuinime.net/2018/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
